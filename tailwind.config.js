9c508129388fcff793713fac0bd19eca25dbeea7
PettersonWilliam <petterson309@gmail.com>
2025-11-03 23:00:04 -0300

    Add README, Tailwind, Vite configuration, and initial styles

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Para todos os arquivos JS, TS, JSX e TSX no diretório src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
